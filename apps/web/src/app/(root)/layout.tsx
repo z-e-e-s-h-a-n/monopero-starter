a32fd076eb3755b2beaad5d7a3f5484a384343e0
Zeeshan Asghar <mzeeshanff10@gmail.com>
2025-10-14 14:38:33 +0400

    Add default styles & ThemeSwitch component

--- apps/web/src/app/(root)/layout.tsx
+++ apps/web/src/app/(root)/layout.tsx
@@ -4,11 +4,11 @@ import Footer from "@/components/layout/Footer";
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div id="root">
+    <>
       <Header />
       <main>{children}</main>
       <Footer />
-    </div>
+    </>
   );
 }
 
